08a2af38a7229d5e307e74ff610a472a644b753d
KirkHietpas <hietpaskirk@gmail.com>
2022-10-02 10:13:56 -0600

    Playing with go

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
+
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	fmt.Println("Hello world");
-	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println(Test);
 }
---
+++ test.go
@@ -0,0 +1,3 @@
+package main
+// same package name within same directory does not reauire import
+var Test = "TESTING"
